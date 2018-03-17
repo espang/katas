c6b70e27db24a4b4dd6ccac65506d8a751001cef
Eike Spang <eike.spang@infosum.com>
2018-03-17 14:34:35 +0000

    implement search

--- kata2/go/chop.go
+++ kata2/go/chop.go
@@ -4,5 +4,16 @@ package chop
 // chop searches value in sorted. Returns a index
 // of value when it is in sorted and -1 otherwise.
 func chop(value int, sorted []int) int {
-	return 0
+	if len(sorted) == 0 {
+		return -1
+	}
+
+	idx := len(sorted)/2
+	if pivot := sorted[idx]; pivot == value {
+		return idx
+	} else if pivot > value {
+		return chop(value, sorted[:idx])
+	} else {
+		return chop(value, sorted[idx+1:])
+	}
 }
